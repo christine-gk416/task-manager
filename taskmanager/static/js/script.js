69898e71cbddba67504c5bcd6623edd38f51d149
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-04-14 13:01:10 +0000

    Connect CSS and JS files

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+// Initialize Materialize navbar
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
