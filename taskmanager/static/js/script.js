3817ae25f2eb76464b83c9fd701bb678d7a14ada
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-04-14 15:23:21 +0000

    Add delete modal for categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+
+
 });
